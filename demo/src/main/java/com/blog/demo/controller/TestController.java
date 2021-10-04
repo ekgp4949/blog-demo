f6663739b4069b9e3f44859ebdb55c7b1d3a1de3
dhk <ekgp4949@gmail.com>
2021-10-04 23:59:18 +0900

    [init] react server 초기화

---
+++ demo/src/main/java/com/blog/demo/controller/TestController.java
@@ -0,0 +1,48 @@
+package com.blog.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.demo.dto.ResponseDTO;
+import com.blog.demo.dto.TestRequestBodyDTO;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+	@GetMapping
+	public String testController() {
+		return "Hello";
+	}
+	
+	@GetMapping("/testGetMapping")
+	public String testControllerWithPath(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! ID " + testRequestBodyDTO.getId() 
+		+ " Message : " + testRequestBodyDTO.getMessage();
+	}
+	
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String> testControllerResponseBody() {
+		List<String> list = new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		return ResponseDTO.<String>builder().data(list).build();
+	}
+	
+	@GetMapping("/testResponseEntity")
+	public ResponseEntity<?> testControllerResponseEntity() {
+		List<String> list = new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		ResponseDTO<String> responseDTO = ResponseDTO.<String>builder().data(list).build(); 
+		return ResponseEntity.badRequest().body(responseDTO);
+	}
+ }
