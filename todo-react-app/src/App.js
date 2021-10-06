1e7e638c9b18a8008c8425611fa665dfaf813ab0
ekgp4949 <ekgp4949@gmail.com>
2021-10-06 23:02:19 +0900

    [MOD] Todo.js 수정

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,14 +1,39 @@
+import React from 'react';
 import './App.css';
 import Todo from './Todo'
 
-function App() {
-  return (
-    <div className="App">
-      <Todo />
-      <Todo />
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
 
-    </div>
-  );
+    this.state = {
+      item: [
+        { id: 0, title: "Hello world 1", done: true },
+        { id: 1, title: "Hello world 2", done: false },
+        { id: 2, title: "Hello world 3", done: true },
+      ]
+    }
+  }
+
+  render() {
+
+    var todoItems = this.state.item.map((item, index) => (
+      <Todo item={item} key={item.id} />
+    ));
+
+    /*
+    for(let i = 0; i < this.state.item.length; i++ ) {
+      todoItems.push((<Todo item={this.state.item[i]} key={this.state.item[i].id} />));
+    }
+    */
+    return (
+      <div className="App">
+        {todoItems}
+      </div>
+    );
+  }
+
 }
 
 export default App;
