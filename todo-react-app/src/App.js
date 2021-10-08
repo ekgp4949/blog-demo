fb68f023e79a440dc0fc1ad62944fc7023ffda07
ekgp4949 <ekgp4949@gmail.com>
2021-10-09 00:13:38 +0900

    [add] config.js 누락 추가

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -56,7 +56,29 @@ class App extends React.Component {
         {todoItems}
       </div>
     );
-  }
+  };
+
+  componentDidMount() {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch("http://localhost:8080/todo", requestOptions)
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          items: response.data
+        })
+      },
+      (error) => {
+        this.setState({ error })
+      })
+      
+  };
+
 
 }
 
