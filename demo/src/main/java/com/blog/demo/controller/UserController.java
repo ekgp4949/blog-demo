a74b12f51c8fe5c816426787cc80d5b4dcf2cf71
ekgp4949 <ekgp4949@gmail.com>
2021-10-09 20:32:53 +0900

    [add] Use 관련 레이어 구현

---
+++ demo/src/main/java/com/blog/demo/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.blog.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.demo.dto.ResponseDTO;
+import com.blog.demo.dto.UserDTO;
+import com.blog.demo.model.UserEntity;
+import com.blog.demo.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+		try {
+			UserEntity user = UserEntity.builder()
+				.email(userDTO.getEmail())
+				.username(userDTO.getUsername())
+				.password(userDTO.getPassword())
+				.build();
+			UserEntity registeredUser = userService.create(user);
+			UserDTO registeredUserDTO = UserDTO.builder()
+				.id(registeredUser.getId())
+				.email(registeredUser.getEmail())
+				.username(registeredUser.getUsername())
+				.build();
+			return ResponseEntity.ok().body(registeredUserDTO);
+			
+		} catch(Exception e) {
+			
+			ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+			return ResponseEntity.badRequest().body(responseDTO);
+		}
+	}
+	
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
+		UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
+		if(user != null) {
+		final UserDTO responseUserDTO = UserDTO.builder()
+			.id(user.getId())
+			.email(user.getEmail())
+			.build();
+			return ResponseEntity.ok(responseUserDTO);
+		} else {
+			ResponseDTO responseDTO = ResponseDTO.builder().error("Login failed").build();
+			return ResponseEntity.badRequest().body(responseDTO);
+		}
+	}
+	
+}
