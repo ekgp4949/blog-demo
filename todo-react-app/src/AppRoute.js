6d3c09e4d75dd47b14fe07b42b3606a9d83c6e90
ekgp4949 <ekgp4949@gmail.com>
2021-10-10 20:52:56 +0900

    [add] Login.js 생성 및 Route 구현

---
+++ todo-react-app/src/AppRoute.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./index.css"
+import App from "./App";
+import Login from "./Login";
+import { Box } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function Copyright() {
+  return (
+    <Typography color="textSecondary" align="center" variant="body2">
+      {"Copyright Ⓒ "} ekgp4949, {new Date().getFullYear()} {"."}
+    </Typography>
+  );
+}
+
+
+class AppRouter extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/">
+                <App />
+              </Route>
+            </Switch>
+          </div>
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default AppRouter;
