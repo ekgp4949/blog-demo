9320ae82f6e14595d31f3d2e1e21f28532c1aaa0
ekgp4949 <ekgp4949@gmail.com>
2021-12-07 22:18:54 +0900

    [add] TodoHistoryControllerTest 작성

--- demo/src/main/java/com/blog/demo/controller/TodoHistoryController.java
+++ demo/src/main/java/com/blog/demo/controller/TodoHistoryController.java
@@ -4,24 +4,25 @@ import com.blog.demo.dto.ResponseDTO;
 import com.blog.demo.model.TodoHistoryEntity;
 import com.blog.demo.service.TodoHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@RestController("todoHistory")
+@RestController
+@RequestMapping("/todoHistory")
 public class TodoHistoryController {
 
     @Autowired
     TodoHistoryService todoHistoryService;
 
-    @GetMapping
-    public ResponseEntity<?> retrieveTodoHistory(@RequestBody LocalDate date, @AuthenticationPrincipal String userId) {
+    @GetMapping("/{date}")
+    public ResponseEntity<?> retrieveTodoHistory(
+            @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+            @AuthenticationPrincipal String userId) {
         List<TodoHistoryEntity> list = todoHistoryService.retrieve(date, userId);
         return ResponseEntity.ok(ResponseDTO.<TodoHistoryEntity>builder().data(list).build());
     }
