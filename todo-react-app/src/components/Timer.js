fee521fcb2677bbdfef8b7cd011097c80fc02b41
ekgp4949 <ekgp4949@gmail.com>
2021-12-18 23:41:22 +0900

    [mod] Timer 개발

--- todo-react-app/src/components/Timer.js
+++ todo-react-app/src/components/Timer.js
@@ -6,10 +6,41 @@ const padNumber = (num, length) => {
 };
 
 class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { leftHour : "00", leftMin: "00", leftSec: "00" };
+    
+  }
+
+
+  getLeftTime = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    console.log(hours, minutes, seconds);
+    return { 
+      leftHour: padNumber((23 - hours) % 24, 2), 
+      leftMin: padNumber((59 - minutes) % 60, 2), 
+      leftSec: padNumber((60 - seconds) % 60, 2) 
+    };
+
+  };
+  
+  componentDidMount() {
+    this.interval = setInterval(() => { this.setState(this.getLeftTime()); }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  
   render() {
     return (
       <Typography variant="h6" color="black">
-        0 : 0 : 0
+        { this.state.leftHour + " : "+ this.state.leftMin + " : " + this.state.leftSec }
       </Typography>
     );
   }
