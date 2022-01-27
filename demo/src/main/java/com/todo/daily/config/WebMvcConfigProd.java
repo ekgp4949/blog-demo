aab49df5d610c68cc526b2ad5b73fa727d47bfdc
ekgp4949 <ekgp4949@gmail.com>
2022-01-27 23:59:50 +0900

    [mod] frontend단 ui 추가

--- demo/src/main/java/com/todo/daily/config/WebMvcConfigProd.java
+++ demo/src/main/java/com/todo/daily/config/WebMvcConfigProd.java
@@ -3,19 +3,12 @@ package com.todo.daily.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Profile("prod")
 @Configuration
 public class WebMvcConfigProd implements WebMvcConfigurer {
 	private final long MAX_AGE_SECS = 3600;
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//		registry.addResourceHandler("/image/**")
-//				.addResourceLocations(uploadPath);
-	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
